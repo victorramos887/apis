b30d1c052202fd93a6117101e3f1231243e3204a
victorramos887 <v.ramos58@hotmail.com>
2022-08-17 08:58:23 -0300

    Criando Banco de dados
    Script para criar o Banco de dados no PostgreSQL automaticamente

--- criando_banco_postgres.py
+++ criando_banco_postgres.py
@@ -1,66 +1,85 @@
+from calendar import c
 import psycopg2 as pg
 
-print('Conectando ...')
+def conectando(host = 'localhost', user = 'postgres', password = 'postgres', database = 'alura'):
+    print('Conectando ...')
 
-try:
-    conn = pg.connect(host = 'localhost',
-                      user = 'postgres',
-                      password = 'postgres',
-                      database = 'alura')
+    try:
+         conn = pg.connect(host = host,
+                        user = user,
+                        password = password,
+                        database = database)
+         conn.autocommit = True
+         return conn
 
-except pg.connect.Error as e:
-    print(e)
+    except pg.connect.Error as e:
+        print(e)
+        return 'Erro'
 
 
-cursor = conn.cursor()
-
+cursor = conectando().cursor()
 #CRIANDO BANCO DE DADOS
 
-cursor.execute("DROP DATABASE IF EXISTS 'jogoteca';")
-cursor.execute("CREATE DATADBASE 'jogoteca';")
-cursor.execute("USER 'jogoteca';")
 
+cursor.execute("DROP DATABASE IF EXISTS jogoteca;")
+cursor.execute("CREATE DATABASE jogoteca;")
+
+database = 'jogoteca'
+
+cursor.close()
+
+cursor = conectando(database=database).cursor()
+
+cursor.execute("""
+               CREATE SCHEMA cursoflask
+    AUTHORIZATION postgres;
+    """)
 
 TABLES = {}
 
-TABLES['Jogos'] = (
-    """
-    CRETE TABLE cursoflask.'jogos' (
-        'id' SERIAL PRIMARY KEY,
-        'nome' VARCHAR(50) NOT NULL,
-        'categoria' VARCHAR(40) NOT NULL,
-        'console' VARCHAR(20) NOT NULL,
-    )
-    """
-)
+TABLES['Jogos'] =  """CREATE TABLE IF NOT EXISTS cursoflask.jogos (
+        id SERIAL PRIMARY KEY,
+        nome VARCHAR(50) NOT NULL,
+        categoria VARCHAR(40) NOT NULL,
+        console VARCHAR(20) NOT NULL
+    )"""
 
+TABLES['Usuarios'] = """CREATE TABLE cursoflask.usuarios(
+      nome varchar(20) NOT NULL,
+      nickname varchar(8) NOT NULL,
+      senha varchar(100) NOT NULL,
+      PRIMARY KEY (nickname)
+      )"""
 
-TABLES['Usuarios'] = (
+# print(TABLES)
+# print('---')
 
-    """
-        CREATE TABLE cursoflask.'jogos' (
-            'id' SERIAL PRIMARY KEY,
-            'nome' VARCHAR(50) NOT NULL,
-            'nickname' VARCHAR(8) NOT NULL UNIQUE,
-            'senha' VARCHAR(100) NOT NULL
-        )
-    """
-)
-
-for table_nome in TABLES.items:
+for table_nome in TABLES.keys():
     table_sql = TABLES[table_nome]
-
+    print(table_sql)
     try:
-        print(f'Criando Table {table_nome}', end = ' ')
+        print(f'Criando Table {table_nome}', end = '\n')
         cursor.execute(table_sql)
     except pg.connect.Error as err:
-        print(e)
+        print(err)
 
 usuario_sql = 'INSERT INTO cursoflask.usuarios (nome, nickname, senha) VALUES (%s, %s, %s)'
 usuarios = [
     ("Bruno Divino", 'BD', 'alohomora'),
-    ('Camila Ferreira', 'Mila', 'paozinho'),
-    ('Guilherme Louro', 'Cake', 'python_eh_vida')
+    ("Camila Ferreira", 'Mila', 'paozinho'),
+    ("Guilherme Louro", 'Cake', 'python_eh_vida')
 ]
 
-cursor.execute()
+cursor.executemany(usuario_sql, usuarios)
+
+
+jogos_sql = 'INSERT INTO cursoflask.jogos (nome, categoria, console) VALUES (%s, %s, %s)'
+jogos = [
+      ('Tetris', 'Puzzle', 'Atari'),
+      ('God of War', 'Hack n Slash', 'PS2'),
+      ('Mortal Kombat', 'Luta', 'PS2'),
+      ('Valorant', 'FPS', 'PC'),
+      ('Crash Bandicoot', 'Hack n Slash', 'PS2'),
+      ('Need for Speed', 'Corrida', 'PS2'),
+]
+cursor.executemany(jogos_sql, jogos)
