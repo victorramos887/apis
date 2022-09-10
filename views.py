0d5ba6328fee21f56d80b97e512af787ef6e9b44
victorramos887 <v.ramos58@hotmail.com>
2022-09-10 00:24:34 -0300

    adicionando imagens

--- views.py
+++ views.py
@@ -50,6 +50,10 @@ def criar():
 
     db.session.add(novo_jogo)
     db.session.commit()
+
+    arquivo = request.files['arquivo']
+    arquivo.save(f'uploads/{arquivo.filename}')
+
     return redirect(url_for('index'))
 
 @app.route('/editar/<int:id>')
